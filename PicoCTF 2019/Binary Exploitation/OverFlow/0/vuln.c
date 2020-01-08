756f61672a80dbaf30cdd0876c203cf8e7dff17d
BOAKGP <19695201+BOAKGP@users.noreply.github.com>
2020-01-08 17:16:23 +0100

    Added handy shellcode, overflow and practice run

---
+++ PicoCTF 2019/Binary Exploitation/OverFlow/0/vuln.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#define FLAGSIZE_MAX 64
+
+char flag[FLAGSIZE_MAX];
+
+void sigsegv_handler(int sig) {
+  fprintf(stderr, "%s\n", flag);
+  fflush(stderr);
+  exit(1);
+}
+
+void vuln(char *input){
+  char buf[128];
+  strcpy(buf, input);
+}
+
+int main(int argc, char **argv){
+  
+  FILE *f = fopen("flag.txt","r");
+  if (f == NULL) {
+    printf("Flag File is Missing. Problem is Misconfigured, please contact an Admin if you are running this on the shell server.\n");
+    exit(0);
+  }
+  fgets(flag,FLAGSIZE_MAX,f);
+  signal(SIGSEGV, sigsegv_handler);
+  
+  gid_t gid = getegid();
+  setresgid(gid, gid, gid);
+  
+  if (argc > 1) {
+    vuln(argv[1]);
+    printf("You entered: %s", argv[1]);
+  }
+  else
+    printf("Please enter an argument next time\n");
+  return 0;
+}
